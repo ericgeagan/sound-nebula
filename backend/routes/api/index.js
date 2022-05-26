46735d0dcd3b76538f7464e52313ea0c52400dde
ericgeagan <ericgeagan@gmail.com>
2022-05-26 15:50:37 -0700

    frontend setup complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,9 +5,9 @@ const { User } = require('../../db/models');
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 
-router.post('/test', function(req, res) {
-  res.json({ requestBody: req.body });
-});
+// router.post('/test', function(req, res) {
+//   res.json({ requestBody: req.body });
+// });
 
 router.use('/session', sessionRouter);
 
