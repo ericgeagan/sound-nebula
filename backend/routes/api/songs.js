84d17cd5422aebd92209e68ca2314cff73442176
ericgeagan <ericgeagan@gmail.com>
2022-05-31 16:35:16 -0700

    update route added

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -28,3 +28,38 @@ router.post(
 		const id = await Song.create(req.body)
 		return res.redirect(`${req.baseUrl}/${id}`)
 }))
+
+// Read route (get all songs)
+router.get(
+	'/', 
+	asyncHandler(async (req, res) => {
+		// Can change this to first X songs later
+		const songs = await Songs.findAll()
+		return res.json(songs)
+}))
+
+// Read route (one song)
+router.get(
+	'/:id',
+	asyncHandler(async (req, res) => {
+		const song = await Song.one(req,params.id)
+		return res.json(song)
+}))
+
+// Update route
+router.put(
+	'/:id', 
+	validateSong, 
+	asyncHandler(async (req, res) => {
+		const song = req.body
+		const id = song.id
+		delete song.id
+		await Song.update(
+			song, 
+			{
+				where: { id },
+				returning: true,
+				plain: true
+			})
+		return id
+}))
